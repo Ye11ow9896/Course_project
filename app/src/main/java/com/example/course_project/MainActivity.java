2119d511e9b7c7c5a50f7b905a9ccffce3b017f3
Ye11ow9896 <kondratenkoalex1994@gmail.com>
2020-11-29 19:12:38 +0300

    добавляю авторизацию пользователя

--- app/src/main/java/com/example/course_project/MainActivity.java
+++ app/src/main/java/com/example/course_project/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.course_project;
 
+import android.content.ContentValues;
 import android.content.Intent;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
@@ -14,37 +15,36 @@ import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity implements OnClickListener {
 
-    DBHelper dbHelper;
+    final String LOG_TAG = "myLogs";
 
+    /*Переменные класса*/
+    DBHelper dbHelper;
     private Button BtnLoginProfile, BtnCreateProfile;
     private EditText textName, textPassword;
-
-
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_main);//привязка в лойауту
 
-        BtnLoginProfile = (Button)findViewById(R.id.btnLoginProfile);
-        BtnLoginProfile.setOnClickListener(this);
+        /*привязка к кнопкам и полям для заполнения по ID*/
+        BtnLoginProfile = (Button) findViewById(R.id.btnLoginProfile);
+        BtnLoginProfile.setOnClickListener(this);//возвращает обратку при нажатии
 
-        BtnCreateProfile = (Button)findViewById(R.id.btnCreateProfile);
-        BtnCreateProfile.setOnClickListener(this);
+        BtnCreateProfile = (Button) findViewById(R.id.btnCreateProfile);
+        BtnCreateProfile.setOnClickListener(this);//возвращает обратку при нажатии
 
-        dbHelper = new DBHelper(this);
-        SQLiteDatabase db = dbHelper.getWritableDatabase();
-        Cursor c = db.query("mytable", null, null, null, null, null, null);
+        textName = (EditText) findViewById(R.id.editTextTextPersonName);
+        textPassword = (EditText) findViewById(R.id.editTextTextPassword);
+
+
+
+        dbHelper = new DBHelper(this);//создаем объекс класса
+
     }
+
     @Override
     public void onClick(View v) {
-
-        //привязываем кнопки по айди
-        BtnLoginProfile = (Button) findViewById(R.id.btnLoginProfile);
-        BtnCreateProfile = (Button) findViewById(R.id.btnCreateProfile);
-        textName = (EditText) findViewById(R.id.NumberOfCar);
-        textPassword = (EditText) findViewById(R.id.Password);
-
 
         switch (v.getId()) {
 
@@ -55,56 +55,37 @@ public class MainActivity extends AppCompatActivity implements OnClickListener {
                 break;
             //По нажатию кнопки переходим в активити профиля
             case R.id.btnLoginProfile:
-                //searchData();
-                Intent intent1 = new Intent(MainActivity.this, ProfileActivity.class);
-                startActivity(intent1);
-                break;
-            }
+                SQLiteDatabase db = dbHelper.getWritableDatabase(); //Метод getWritable... создает БД или, если она создана, то предоставляет доступ на запись/чтение
+                Cursor c = db.query("mytable", null, null, null, null, null, null);//создаем таблицу в БД
+                /*переменные для сравнения при поиске данных пароля и номера авто при авторизации*/
+                String name = textName.getText().toString();
+                String pass = textPassword.getText().toString();
+                    /*Авторизация*/
+                    if (c != null) {//курсор не нулл
+                            if (c.moveToFirst()) {//если таблица не пустая - двигаем курсор на первую строку таблицы
+                                /*задаем интовые переменные, которые соответствуют номеру колонок*/
+                                int carNumberColIndex = c.getColumnIndex("carNumber");
+                                int passwordColIndex = c.getColumnIndex("password");
+                                int idColIndex = c.getColumnIndex("id");
+
+                                do {//проверяем есть ли введенные данные в нашей БД и прогоняем по все базе, пока не найдем совпадения
+                                    Log.d(LOG_TAG, c.getString(carNumberColIndex));
+                                    Log.d(LOG_TAG, c.getString(passwordColIndex));
+                                        //если пароль и номер авто совпадают с данными из БД то переходим в активити профиля
+                                        if (name.equals(c.getString(carNumberColIndex))&&pass.equals(c.getString(passwordColIndex))) {
+                                                Intent intent1 = new Intent(MainActivity.this, ProfileActivity.class);
+                                                intent1.putExtra("row", c.getString(idColIndex));//Передача данных id из базы в др активити
+                                                startActivity(intent1);//переход в активити
+                                                break;
+                                            }
+
+                                } while (c.moveToNext());//конец таблица БД
+                            }
+                            c.close();//закрытие курсора
+                    } else
+            Log.d(LOG_TAG, "Cursor is null");
+            dbHelper.close();//закрываем объект БД
+            break;
         }
-
-
-        //BtnCreateProfile.setOnClickListener(
-        //        new View.OnClickListener(){
-        //            @Override
-        //
-        //        }
-        //);
-
-
-   //    BtnCreateProfile.setOnClickListener(
-
-   //        new View.OnClickListener(){
-   //            @Override
-   //            public void onClick(View v) {
-   //
-   //            }
-   //        }
-   //    );
-   //}
-   //void searchData() {//функция поиска данных пароля и мени для авторизаци
-   //
-
-   //    if (c.moveToFirst()) {
-
-   //        // определяем номера столбцов по имени в выборке
-   //        int idColIndex = c.getColumnIndex("id");
-   //        int userNameColIndex = c.getColumnIndex("userName");
-   //        int carModelColIndex = c.getColumnIndex("carModel");
-   //        int carNumberColIndex = c.getColumnIndex("carNumber");
-   //        int odoValueColIndex = c.getColumnIndex("odoValue");
-   //        int passwordColIndex = c.getColumnIndex("password");
-
-   //        do {
-   //            // получаем значения по номерам столбцов и пишем все в лог
-   //             textName.setText(String.valueOf(c.getString(userNameColIndex)));
-   //             textPassword.setText(String.valueOf(c.getString(passwordColIndex)));
-   //            // переход на следующую строку
-   //            // а если следующей нет (текущая - последняя), то false - выходим из цикла
-   //        } while (c.moveToNext());
-   //    } else
-   //    c.close();
-
-   //}
-
-
+    }
 }
